163dbf9ac06fa73f64a684b3d9d90acf905b0276
Wyatt Joyner <wcj3te@virginia.edu>
2019-06-08 08:16:59 -0400

    adding gamemanager and gamekit

---
+++ PSJambo2019/Assets/Scripts/Managers/FXSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXSpawner : MonoBehaviour
+{
+
+    public static FXSpawner instance;
+
+    #region effects
+    //Add sound effect vars here
+
+
+    #endregion
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(this);
+        }
+    }
+
+    public void SpawnEffect(GameObject obj, Vector3 location) {
+        Instantiate(obj, location, Quaternion.identity, null);
+    }
+}
