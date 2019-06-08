368e345d309df12f5eedbbb470da0e9e945d3539
Simon Whittle <smw2ed@virginia.edu>
2019-06-08 12:28:14 -0400

    Added some spider eyes bullshit

---
+++ PSJambo2019/Assets/Convos/DummyBoi.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public enum ItemType {  SpiderEyes }
+
+public class DummyBoi : MonoBehaviour
+{
+
+    public Flowchart flowchart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool HasItem(ItemType _item)
+    {
+        return Random.value > 0.5f;
+    }
+
+    public void Continue()
+    {
+        Debug.Log("CONTINUE");
+    }
+
+    public void Get()
+    {
+        Debug.Log("GET");
+    }
+
+    public void Give()
+    {
+        Debug.Log("GIVE");
+    }
+
+    public void Leave()
+    {
+        Debug.Log("LEAVE");
+    }
+
+    public void Finish()
+    {
+        Debug.Log("FINISH");
+    }
+
+    public void Talk()
+    {
+        flowchart.ExecuteBlock("s-TALK");
+    }
+}
