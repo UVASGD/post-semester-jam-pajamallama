b06da3959c2742aaba627ece90817139e7754d80
Wyatt Joyner <wcj3te@virginia.edu>
2019-06-07 20:23:38 -0400

    Add standard assets and interactable framework

---
+++ PSJambo2019/Assets/Scripts/Pauser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Pauser : MonoBehaviour
+{
+    public static Pauser instance;
+    HashSet<IPausable> pausables = new HashSet<IPausable>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (!instance)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    public void Pause()
+    {
+        pausables = new HashSet<IPausable>(FindObjectsOfType<MonoBehaviour>().OfType<IPausable>());
+    }
+}
