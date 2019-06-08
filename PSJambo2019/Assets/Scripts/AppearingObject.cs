84890e7caf71816da75682a62079e881a2d37e27
Jackson Ekis <jhe3cd@virginia.edu>
2019-06-08 00:12:43 -0700

    Added a Secret Stage
    Hello, this is an anonymous developer, and this is my secret stage. You guys definitely don't know who I am.
    
    Enjoy my glorious contribution of an extremely stale meme because I have no creativity of my own, and I just wanted to say hello.
    
    Hint: The default Interact button is "F".

---
+++ PSJambo2019/Assets/Scripts/AppearingObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearingObject : MonoBehaviour
+{
+    [SerializeField]
+    private Renderer appearingRenderer;
+
+    [SerializeField]
+    private float distanceToAppear = 1.0f;
+
+    [SerializeField]
+    private GameObject target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (null != target)
+        {
+            float distance = (target.transform.position - gameObject.transform.position).magnitude;
+
+            if (distance < distanceToAppear)
+            {
+                appearingRenderer.enabled = true;
+            } else
+            {
+                appearingRenderer.enabled = false;
+            }
+        }
+    }
+}
